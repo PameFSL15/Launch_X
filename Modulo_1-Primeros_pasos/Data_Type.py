3357cb8a921b9bbe772d0b86831b1492c54aec9d
PameFSL15 <pame501@yahoo.com.mx>
2022-02-08 15:13:54 -0600

    Kata modulo 1 - revision de material

---
+++ Modulo_1-Primeros_pasos/Data_Type.py
@@ -0,0 +1,8 @@
+# Declaramos un conjunto de variables 
+planetas_en_el_sistema_solar = 8 # int, plutón era considerado un planeta pero ya es muy pequeño
+distancia_a_alfa_centauri = 4.367 # float, años luz
+puede_despegar = True
+transbordador_que_aterrizo_en_la_luna = "Apollo 11" #string
+
+# Utilizamos la funcion type para ver el tipo de dato de la variable 
+print(type(distancia_a_alfa_centauri))
---
+++ Modulo_1-Primeros_pasos/Programa.py
@@ -0,0 +1,16 @@
+# program.py
+
+sum = 1 + 2
+print('La suma de 1 + 2 es: ', sum)
+
+# Multiplicación
+product = sum * 2
+print('\nMultiplicado por 2 es: ', product)
+
+
+print('\n*********************************')
+print("Bienvenido al programa Launch X")
+name = input("Introduzca su nombre ")
+
+#Imprime mensaje utilizando la variable de entrada (name)
+print("Le damos la más cordial bienvenida: " + name)
