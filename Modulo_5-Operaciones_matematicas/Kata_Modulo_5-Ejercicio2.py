972bd30b7625ea2db872112c8b3a9545b5d8cfa2
PameFSL15 <pame501@yahoo.com.mx>
2022-02-11 16:50:20 -0600

    Kata modulo 5

---
+++ Modulo_5-Operaciones_matematicas/Kata_Modulo_5-Ejercicio2.py
@@ -0,0 +1,9 @@
+# Ejercicio 2: convierte cadenas en números y usa valores absolutos
+
+
+distancia_p1 = int( input('Ingresa la distancia del planeta 1: ') )
+distancia_p2 = int(input('Ingresa la distancia del planeta 2: ') )
+
+
+print('La distancia en Km entre los dos planetas es: ', abs(distancia_p2 - distancia_p1))
+print('La distancia en millas entre los dos planetas es: ', abs(distancia_p2 - distancia_p1)*0.621)
